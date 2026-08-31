1751873e4d4e6fbc08c3c33b52c020009e4b5788
beckriley <145520167+beckriley@users.noreply.github.com>
2026-08-31 15:41:34 -0700

    Update InstructionFetchUnit.v

--- InstructionFetchUnit.v
+++ InstructionFetchUnit.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 ////////////////////////////////////////////////////////////////////////////////
-// Team Members: Beck Riley, Erick Figueroa
+// Team Members: Beck Riley, Erick Figueroa, Earl Sangalang
 // Overall percent effort of each team meber: 
 // 
 // ECE369A - Computer Architecture
