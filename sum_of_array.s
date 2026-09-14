3af8193c7e516dd56da51601688d7f453dac3442
Earls3 <132307780+Earls3@users.noreply.github.com>
2026-09-14 16:49:25 -0700

    Update address locations and register values in comments

--- sum_of_array.s
+++ sum_of_array.s
@@ -7,14 +7,14 @@
 #
 # Insert your answers below each question:-
 # 1(a) What is the address location of the first element of the array, X.
-#      10010000
+#      0x10010000
 # 1(b) What is the address location of the last element (element 7) of the array, X.
-#      10010010
+#      0x1001000C
 # 2.   What are the values at location 'SUM' as the program executes?
 #    SUM is zero until loop ends then equals $s1 being 1
 # 3.   Traverse the program in single-step mode. Write down the intermediate 
 #      values stored in register $s1 as the program progresses.
-#    $s1 = 0, -1, -6, 1
+#    $s1 = 0, -2, -6, 1
 # 4.   What is the purpose of register $t0 in this program?
 #    The purpose of register $t0 is to point to the address and next address to load
 #
