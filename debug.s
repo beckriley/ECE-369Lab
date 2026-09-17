5f74899027d46d38adb7ac2904933d4ad15b99e0
Earls3 <132307780+Earls3@users.noreply.github.com>
2026-09-16 20:50:32 -0700

    Update comments and fix formatting in debug.s

--- debug.s
+++ debug.s
@@ -1,7 +1,8 @@
 # Exercise 2
 #  Max Score: 9 points
 #
-# Students: 
+# Students: Beck Riley, Erick Figueroa, Earl Sangalang
+# Overall percent effort of each team member: 33%
 #
 # 'count_occurence.a' - count the occurrences of a specific character in string 
 # "str". Indexed addressing is used to access the array elements.
