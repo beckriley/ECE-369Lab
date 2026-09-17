61462267b3db3edb3fa7e459569a1055d974d7a6
Earls3 <132307780+Earls3@users.noreply.github.com>
2026-09-16 20:52:01 -0700

    Update student names and effort percentage

--- sum_of_array.s
+++ sum_of_array.s
@@ -2,8 +2,8 @@
 # 'sum_of_array.s' - This program performs the sum of the array
 #  Max Score: 5 points
 #
-# Students: 
-#
+# Students: Beck Riley, Erick Figueroa, Earl Sangalang
+# Overall percent effort of each team member: 33%
 #
 # Insert your answers below each question:-
 # 1(a) What is the address location of the first element of the array, X.
